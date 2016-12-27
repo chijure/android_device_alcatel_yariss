2bc248467320d895a681874714da2c946522d4c1
Chijure <chijure100x100to@gmail.com>
2016-12-27 15:40:56 -0500

    Adaptation for TWRP thanks @kirito96 Signed-off-by: Jhuan Reategui <chijure100x100to@gmail.com>

--- cm.mk
+++ cm.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/alcatel/yariss/device_yariss.mk)
 
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := yariss
 PRODUCT_NAME := cm_yariss
--- device_yariss.mk
+++ device_yariss.mk
@@ -22,3 +22,18 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_yariss
 PRODUCT_DEVICE := yariss
+
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0 \
+ro.allow.mock.location=1 \
+persist.mtk.aee.aed=on \
+ro.debuggable=1 \
+persist.service.acm.enable=0 \
+persist.sys.usb.config=mass_storage \
+ro.bootloader.mode=download \
+ro.mount.fs=EXT4 \
+ro.persist.partition.support=no
+
+PRODUCT_COPY_FILES_OVERRIDES += \
+    root/fstab.goldfish \
+    root/init.goldfish.rc \
+    recovery/root/fstab.goldfish
